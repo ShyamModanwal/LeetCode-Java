51ca283a8a031f7dd550817f7365b9a8e8965c97
Shyam Modanwal <shyammodanwal705@gmail.com>
2026-07-18 15:41:26 +0530

    Day : 4 String Easy and Median Solution

---
+++ Strings/003_Longest_Substring_Without_Repeatition.java
@@ -0,0 +1,28 @@
+ class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int left = 0;
+        int max_length = 0;
+
+        Set<Character> charSet = new HashSet<>();
+
+        for (int right = 0; right < n ; right++)
+        {
+            if(!charSet.contains(s.charAt(right)))
+            {
+                charSet.add(s.charAt(right));
+                max_length = Math.max(max_length , right - left + 1);
+            }
+            else {
+                while (charSet.contains(s.charAt(right)))
+                {
+                    charSet.remove(s.charAt(left));
+                    left++;
+                }
+                charSet.add(s.charAt(right));
+            }
+        }
+        return max_length;
+        
+    }
+}
