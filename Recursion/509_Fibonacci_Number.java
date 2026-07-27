d593123c8edcb5b3cfb4072684c18fe303770044
Shyam Modanwal <shyammodanwal705@gmail.com>
2026-07-27 16:33:08 +0530

    Slove tree ans recrsion problem

---
+++ Recursion/509_Fibonacci_Number.java
@@ -0,0 +1,19 @@
+ class Solution {
+    public int fib(int n) {
+        int a = 0;
+        int b = 1;
+
+        if (n <= 1)
+        return n;
+        
+        for (int i = 2; i <= n; i++)
+        {
+            int temp = b;
+            b = a + b;
+            a = temp;
+
+        }
+        return b;
+        
+    }
+}
