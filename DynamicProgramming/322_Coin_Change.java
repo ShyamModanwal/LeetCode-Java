3a87ee3bdf62d33f03db1cde5bf641d5853a6732
Shyam Modanwal <shyammodanwal705@gmail.com>
2026-08-02 20:39:16 +0530

    Problem solved

---
+++ DynamicProgramming/322_Coin_Change.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+
+        if (amount < 0)
+        return 0;
+
+        int[] minCoinDp = new int[amount + 1];
+
+        for (int i = 1; i <= amount; i++)
+        {
+            minCoinDp[i] = Integer.MAX_VALUE;
+
+            for (int coin : coins)
+            {
+                if (coin <= i && minCoinDp[i - coin] != Integer.MAX_VALUE)
+                minCoinDp[i] = Math.min(minCoinDp[i] , 1 + minCoinDp[i - coin]);
+            }   
+        }
+        if (minCoinDp[amount] == Integer.MAX_VALUE)
+        {
+            return -1;
+        }
+        return minCoinDp[amount];
+        
+    }
+}
