53979321a7288fc13555bc4a9b2a57a0f30b3639
Shyam Modanwal <shyammodanwal705@gmail.com>
2026-08-08 23:51:46 +0530

    Start Trees Problem after Stack

---
+++ Trees/102_Binary_Tree_Level_Order_Traversal.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null){
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty())
+        {
+            int levelSize = queue.size();
+
+            List<Integer> currentLevel = new ArrayList<>();
+
+            
+            for (int i = 0; i < levelSize; i++)
+            {
+                TreeNode treeNode = queue.poll();
+
+                currentLevel.add(treeNode.val);
+
+                if (treeNode.left != null)
+                {
+                    queue.offer(treeNode.left);
+                } 
+
+                if (treeNode.right != null)
+                {
+                    queue.offer(treeNode.right);
+                }
+
+            }
+            result.add(currentLevel);
+        }
+        return result;
+        
+    }
+}
